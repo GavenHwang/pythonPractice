62c88ecbda0056ecd272681d5abaa6834fb64a80
HuangLeilei <leilei.huang@hand-china.com>
2019-02-19 10:02:08 +0800

    first commit

---
+++ 07_自带电池.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import sys
+# sys.path.append('D:/Users/Gaven/PycharmProjects/PythonPractice/hello.py')
+# sys.path.remove('D:/Users/Gaven/PycharmProjects/PythonPractice/hello.py')
+#
+# import hello
+# import hello
+# # import两次hello但只会打印出一次“Hello World!”,也就是说模块在第一次导入到程序中时被执行,这样可以避免重复载入
+#
+# hello.hello()
+# hello.test()
+
+import sys, pprint
+
+pprint.pprint(sys.path)
