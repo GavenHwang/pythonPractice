3024988b5edc4385df5904416a0c6c0f3f38d8c8
HuangLeilei <leilei.huang@hand-china.com>
2019-03-05 14:38:10 +0800

    文件和流

--- 02_抽象.py
+++ 02_抽象.py
@@ -1,3 +1,28 @@
+# -*- coding: utf-8 -*-
+
+# def p(a=3, b=2):
+#     a |= b
+#     print(a, b)
+#
+# p()
+#
+# list1 = [{'a': 1, 'b': 2}, {'a': 11, 'b': 22}]
+# list2 = [x.get('a') for x in list1]
+# print(list2)
+#
+# ##结果： [1, 11]
+#
+#
+# class A:
+#     def a(self):
+#         print('A')
+#
+# class B(A):
+#     def b(self):
+#         print('B')
+#
+# B().a()
+
 # 斐波那契数列
 # def fun_fibs(num):
 #     """输出斐波那契数列的前num项"""
--- 05_魔法方法、属性和迭代器.py
+++ 05_魔法方法、属性和迭代器.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-
 # def checkIndex(key):
 #     if not isinstance(key, int):
 #         raise TypeError
@@ -133,7 +132,58 @@
 # a.size = (10, 15)
 # print(a.width, a.height, a.size)
 
-
+# ********************* 迭代器与生成器 ***************************
+# import sys
+# # 输出迭代器的下一个元素
+# list = [6, 2, 3, 4]
+# it = iter(list)
+# print(next(it))
+#
+# # 利用迭代器对象遍历
+# list = [1, 2, 3, 5]
+# it = iter(list)
+# for x in it:
+#     print(x, end=" ")
+#
+#
+# # 类实现迭代功能
+# class MyNumbers:
+#     def __iter__(self):
+#         self.a = 1
+#         return self
+#
+#     def __next__(self):
+#         if self.a <= 20:
+#             x = self.a
+#             self.a += 1
+#             return x
+#         else:
+#             raise StopIteration
+#
+#
+# myclass = MyNumbers()
+# myiter = iter(myclass)
+# for x in myiter:
+#     print(x, end=' ')
+# print()
+#
+# # 生成器函数 - 斐波那契
+# def fibonacci(n):
+#     a, b, counter = 0, 1, 0
+#     while True:
+#         if (counter > n):
+#             return
+#         yield a
+#         a, b = b, a + b
+#         counter += 1
+#
+#
+# f = fibonacci(10)  # f 是一个迭代器，由生成器返回生成
+# while True:
+#     try:
+#         print(next(f), end=" ")
+#     except StopIteration:
+#         sys.exit()
 # 迭代器       python3使用__next__而不是next（）
 # class Fibs:
 #     def __init__(self):
