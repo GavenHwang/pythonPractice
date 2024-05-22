1da5c94fdb376c50d3bab55564772413229b4271
huangll <huangll@sugon.com>
2024-05-22 09:13:33 +0800

    2024

--- tools/sugon/get_all_stuff_email.py
+++
@@ -1,8 +0,0 @@
-# -*- coding:utf-8 -*-
-with open("all_stuff_email.txt", 'r', encoding='utf-8') as fr:
-    lines = fr.readlines()
-txt = ""
-for line in lines:
-    stuff = line.split("\t")
-    txt += '"%s" <%s>, ' % (stuff[1], stuff[4])
-print(txt)
